104372a23a6b9f367197c00d1d479202e20b9fab
tworkman512 <tworkman512@gmail.com>
2018-09-21 00:31:48 +0000

    clean up v4

--- src/ListFavoriteMovies.js
+++ src/ListFavoriteMovies.js
@@ -3,26 +3,26 @@ import React, { Component } from 'react'
 class ListFavoriteMovies extends Component {
 render() {
   console.log('Props', this.props);
-  const users = this.props.users
-  const profiles = this.props.profiles
-  const movies = this.props.movies
+  const users = this.props.users;
+  const profiles = this.props.profiles;
+  const movies = this.props.movies;
   
-	return (
-      <div className='favorite-movie-wrapper'>
-      	<ul className='movie-list'>
-			{profiles.map( (profile) => {
-              const userName = users[profile.userID].name;
-			  const favoriteMovie = movies[profile.favoriteMovieID].name;
-              return (
-              	<li key={profile.id}>
-                {`${userName}\'s favorite movie is "${favoriteMovie}".`}
-				</li>
-              )
-            })}	
-      	</ul>
-      </div>
-      )
-	}
+  return (
+    <div className='favorite-movie-wrapper'>
+      <ul className='movie-list'>
+    {profiles.map( (profile) => {
+    const userName = users[profile.userID].name;
+    const favoriteMovie = movies[profile.favoriteMovieID].name;
+    return (
+      <li key={profile.id}>
+      {`${userName}\'s favorite movie is "${favoriteMovie}".`}
+      </li>
+    )
+    })}
+      </ul>
+    </div>
+  )
+  }
 }
 
 export default ListFavoriteMovies
